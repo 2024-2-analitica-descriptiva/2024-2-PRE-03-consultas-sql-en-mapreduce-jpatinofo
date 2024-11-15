5941d5a5321e4110d453b9d93a9f0b017f609119
Juan Esteban Patino Forero <jpatinofo@unal.edu.co>
2024-11-15 17:07:44 -0500

    jpatinofo - Lab3

--- homework/queries.py
+++ homework/queries.py
@@ -3,12 +3,146 @@
 # pylint: disable=broad-exception-raised
 # pylint: disable=import-error
 
+from .mapreduce import run_mapreduce_job  # type: ignore
+
+#
+# Columns:
+# total_bill, tip, sex, smoker, day, time, size
+#
+
+
+#
+# SELECT *, tip/total_bill as tip_rate
+# FROM tips;
+#
+def mapper_query_1(sequence):
+    """Mapper"""
+    result = []
+    for index, (_, row) in enumerate(sequence):
+        if index == 0:
+            result.append((index, row.strip() + ",tip_rate"))
+        else:
+            row_values = row.strip().split(",")
+            total_bill = float(row_values[0])
+            tip = float(row_values[1])
+            tip_rate = tip / total_bill
+            result.append((index, row.strip() + "," + str(tip_rate)))
+    return result
+
+
+def reducer_query_1(sequence):
+    """Reducer"""
+    return sequence
+
+
+# SELECT *
+# FROM tips
+# WHERE time = 'Dinner';
+#
+def mapper_query_2(sequence):
+    """Mapper"""
+    result = []
+    for index, (_, row) in enumerate(sequence):
+        if index == 0:
+            result.append((index, row.strip() + ",tip_rate"))
+        else:
+            row_values = row.strip().split(",")
+            if row_values[5] == "Dinner":
+                result.append((index, row.strip()))
+    return result
+
+
+def reducer_query_2(sequence):
+    """Reducer"""
+    return sequence
+
+
+# SELECT *
+# FROM tips
+# WHERE time = 'Dinner' AND tip > 5.00;
+def mapper_query_3(sequence):
+    """Mapper"""
+    result = []
+    for index, (_, row) in enumerate(sequence):
+        if index == 0:
+            result.append((index, row.strip()))
+        else:
+            row_values = row.strip().split(",")
+            if row_values[5] == "Dinner" and float(row_values[1]) > 5.00:
+                result.append((index, row.strip()))
+    return result
+
+
+def reducer_query_3(sequence):
+    """Reducer"""
+    return sequence
+
+
+#
+# SELECT *
+# FROM tips
+# WHERE size >= 5 OR total_bill > 45;
+#
+def mapper_query_4(sequence):
+    """Mapper"""
+    result = []
+    for index, (_, row) in enumerate(sequence):
+        if index == 0:
+            result.append((index, row.strip()))
+        else:
+            row_values = row.strip().split(",")
+            if int(row_values[6]) >= 5 or float(row_values[0]) > 45:
+                result.append((index, row.strip()))
+    return result
+
+
+def reducer_query_4(sequence):
+    """Reducer"""
+    return sequence
+
+
+# SELECT sex, count(*)
+# FROM tips
+# Group by Sex;
+#
+def mapper_query_5(sequence):
+    """Mapper"""
+    result = []
+    for index, (_, row) in enumerate(sequence):
+        if index == 0:
+            continue
+        row_values = row.strip().split(",")
+        result.append((row_values[2], 1))
+    return result
+
+
+def reducer_query_5(sequence):
+    """Reducer"""
+    counter = dict()
+    for key, value in sequence:
+        if key not in counter:
+            counter[key] = 0
+        counter[key] += value
+
+    return list(counter.items())
+
 
 #
 # ORQUESTADOR:
 #
 def run():
     """Orquestador"""
+
+    for num_query in range(1, 6):
+        # Query
+        print(f"Query {num_query}")
+
+        run_mapreduce_job(
+            mapper=eval(f"mapper_query_{num_query}"),
+            reducer=eval(f"reducer_query_{num_query}"),
+            input_directory="files/input",
+            output_directory=f"files/query_{num_query}",
+        )
 
 
 if __name__ == "__main__":
